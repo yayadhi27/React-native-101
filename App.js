4189d43ea15b9465db4279c25563c106d28e972f
yayadhi27 <yayadhi2703@gmail.com>
2024-01-28 17:41:01 +0530

    Inital changes

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.baseText}>React Native 101</Text>
     </View>
   );
 }
@@ -13,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFDD0',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  baseText: {
+    color: '#964B00',
+    fontWeight: 'bold',
+    fontSize: 40,
   },
 });
+
